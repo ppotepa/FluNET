1169843f7941516eef6c2deedcef5b4fa96dcfab
Paweł Potępa <potepapawel@gmail.com>
2026-08-13 23:30:31 +0200

    Provide host variable context to type checker

--- src/FluNET.Engine/Compilation/TypedProgramTypeValidator.cs
+++ src/FluNET.Engine/Compilation/TypedProgramTypeValidator.cs
@@ -1,5 +1,22 @@
+using FluNET.Language;
+using FluNET.Variables;
+
 namespace FluNET.Compilation;
 
 public sealed partial class TypedProgramTypeValidator
 {
+    private readonly LanguageSnapshot? _language;
+    private readonly IVariableResolver? _variables;
+
+    public TypedProgramTypeValidator()
+    {
+    }
+
+    public TypedProgramTypeValidator(
+        LanguageSnapshot language,
+        IVariableResolver variables)
+    {
+        _language = language ?? throw new ArgumentNullException(nameof(language));
+        _variables = variables ?? throw new ArgumentNullException(nameof(variables));
+    }
 }
