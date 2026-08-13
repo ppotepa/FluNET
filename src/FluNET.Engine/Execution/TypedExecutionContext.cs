555c870920af95b65b2a6bf064e4bf100d5497e1
Paweł Potępa <potepapawel@gmail.com>
2026-08-13 23:01:13 +0200

    Expose typed program context helpers

--- src/FluNET.Engine/Execution/TypedExecutionContext.cs
+++ src/FluNET.Engine/Execution/TypedExecutionContext.cs
@@ -1,6 +1,16 @@
+using FluNET.Compilation;
+
 namespace FluNET.Execution;
 
 internal static class TypedExecutionContext
 {
     internal const string Key = "FluNET.TypedProgram";
+
+    public static TypedProgram? GetTypedProgram(this ExecutionContext context) =>
+        context.Data.TryGetValue(Key, out object? value)
+            ? value as TypedProgram
+            : null;
+
+    public static void SetTypedProgram(this ExecutionContext context, TypedProgram program) =>
+        context.Data[Key] = program ?? throw new ArgumentNullException(nameof(program));
 }
